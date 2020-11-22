0b4aa81b2d378da53238d3d1e5eb9f9b0293e917
Tanja <ta.heinrich@online.de>
2020-11-22 19:18:59 +0100

    Specified path to set title from "laravel" to "Team Spark"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-    <meta charset="utf-8">
+      <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="#############">
@@ -21,7 +21,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('webpage.title') {{ config('app.name', 'Team Spark') }}</title>
+    <title>@yield('webpage.title') {{ config('/config/app.name', 'Team Spark') }}</title>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
