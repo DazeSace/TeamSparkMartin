00f5c30873f1b86464453e32ddc0d1b10afbca77
dazesace <dazemcblaze@gmail.com>
2021-01-02 16:11:36 +0100

    - seeded users now have random profile pics

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'dob' => $faker->date,
+        'avatar' => $faker->randomElement(['/uploads/user/profil1.png', '/uploads/user/profil2.png', '/uploads/user/profil3.png', '/uploads/user/profil4.png', '/uploads/user/profil5.png', '/uploads/user/profil6.png']),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
